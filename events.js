5f146d23d54026d284499f32c03124c7e37763db
Pablo Fernandez <pablo.fernandezfernandez@outlook.com>
2022-10-11 19:00:05 +0200

    work on first branch

--- events.js
+++ events.js
@@ -2,10 +2,11 @@ window.addEventListener("load", function() {
     document.querySelector(".header-right").addEventListener("mouseover", function(event){
         console.log(event.offsetX);
     });
+    mouseTrail(5,5);
 });
 
 const mouseTrail = (x, y) => {
-    
+    return x + y
 }
 
 // https://codepen.io/Mertl/pen/XWdyRwJ
