b364c6c2f8ff41887148c2d8ef1b1d77546deb09
Pablo Fernandez <pablo.fernandezfernandez@outlook.com>
2022-10-14 19:53:56 +0200

    mouse trail day 4

--- events.js
+++ events.js
@@ -2,6 +2,8 @@ window.addEventListener("load", function() {
     const canvas = document.getElementById("canvas-element");
     const context = canvas.getContext("2d");
     const TRAIL_LENGTH = 20
+    const RADIUS = 12;
+    const REDUCE_RADIUS = 0.5;
     let mouseTrail = [];
     let trailColours = getColours(TRAIL_LENGTH);
     let stopMouse;
@@ -16,16 +18,27 @@ window.addEventListener("load", function() {
         if (mouseTrail.length > TRAIL_LENGTH) mouseTrail.shift();
         // Draw trail
         let i = 0;
+        let radius = RADIUS;
         mouseTrail.forEach(position => {
-            addCircle(position["x"],position["y"], context, trailColours[i]);
+            addCircle(position["x"],position["y"]+(i-TRAIL_LENGTH), context, trailColours[i], radius);
+            addCircle(position["x"],position["y"]+(TRAIL_LENGTH-i), context, trailColours[i], radius);
             i++;
-        });     
+            radius -= REDUCE_RADIUS;
+        });    
         // If mouse stops --> time gets to 0 and deletes trail leaving just the mouse pointer's circle
         let x = 0;
+        radius = RADIUS;
+        let mouseRadius = RADIUS - (REDUCE_RADIUS*TRAIL_LENGTH);
         stopMouse = setInterval(() => {
-            if (x < mouseTrail.length) addCircle(mouseTrail[x]["x"],mouseTrail[x]["y"], context, "black"); // Removes trail starting from end 
+            if (x < mouseTrail.length) {
+                addCircle(mouseTrail[x]["x"],mouseTrail[x]["y"]+(x-TRAIL_LENGTH), context, "black", radius); // Removes trail starting from end 
+                addCircle(mouseTrail[x]["x"],mouseTrail[x]["y"]+(TRAIL_LENGTH-x), context, "black", radius); // Removes trail starting from end 
+                addCircle(mousePosition["x"],mousePosition["y"], context, trailColours.at(-1), mouseRadius); // Adds the mouse trail "head"
+                radius -= REDUCE_RADIUS;
+            } else {
+                mouseTrail = [];
+            }
             x++;
-            addCircle(mousePosition["x"],mousePosition["y"], context, trailColours.at(-1)); // Adds the mouse trail "head"
         }, 1000 * 0.025);
     });
 
@@ -37,13 +50,26 @@ window.addEventListener("load", function() {
     
 });
 
+/*const drawPath = (context, trail) => {
+    let controlPoint1 = trail.at(trail.length/2);
+    let controlPoint2 = trail.at((trail.length/3)*2);
+
+    context.fillStyle = "red";
+    context.beginPath();
+    context.moveTo(trail.at(-1)["x"], trail.at(-1)["y"]);
+    context.lineTo(trail[0]["x"], trail[0]["y"]);
+    context.bezierCurveTo(controlPoint1["x"], controlPoint1["y"], controlPoint2["x"], controlPoint2["y"], trail[0]["x"], trail[0]["y"]); 
+    context.lineTo(trail.at(-1)["x"], trail.at(-1)["y"]);
+    context.closePath();
+    context.fill();
+}*/
+
+
 // Draw a circle in canvas
-const addCircle = (x, y, context, colour) => {
-    const RADIUS = 7.5;
-    
+const addCircle = (x, y, context, colour, radius) => {   
     context.fillStyle = colour;
     context.beginPath();
-    context.arc(x, y, RADIUS, 0, 2 * Math.PI); // Draw circle
+    context.arc(x, y, radius, 0, 2 * Math.PI); // Draw circle
     context.closePath();
     context.fill();
 }
